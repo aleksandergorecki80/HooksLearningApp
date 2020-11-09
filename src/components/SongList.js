2e8576838d0d2a9e061de93be7ed6b32e6cd510d
Aleksander <aleksandergorecki80@gmail.com>
2020-11-09 13:20:16 +0000

    Set up an app

---
+++ src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+    return ( 
+        <div className="song-list">
+            <ul>
+                <li>Szła dzieweczka</li>
+                <li>Przybyli ułani</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default SongList;
