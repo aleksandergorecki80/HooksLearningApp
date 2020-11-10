02f795fd8315d97dd18be60516f38a4fed1251aa
Aleksander <aleksandergorecki80@gmail.com>
2020-11-10 02:15:01 +0000

    useState Hook

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const SongList = () => {
+    const [songs, setSongs] = useState([
+        { title: 'Tancowała igła z nitką', id: 1},
+        { title: 'Lokomotywa', id: 2},
+        { title: 'Reksio', id: 3},
+    ]);
+    const addSong = () => {
+         setSongs( [ ...songs, { title: 'Bonifacy i Filemon', id: uuidv4() } ] );
+    }
     return ( 
         <div className="song-list">
             <ul>
-                <li>Szła dzieweczka</li>
-                <li>Przybyli ułani</li>
+                {songs.map((song) => {
+                    return (
+                        <li key={song.id}>{song.title}</li>
+                    )
+                })}
             </ul>
+            <button onClick={addSong}>Add a song</button>
         </div>
      );
 }
