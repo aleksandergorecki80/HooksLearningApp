a352e4663297ed26a98d3c611ba448437b710e1b
Aleksander <aleksandergorecki80@gmail.com>
2020-11-11 01:48:36 +0000

    useState with form

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import NewSongForm from './NewSongForm';
 
 
 const SongList = () => {
@@ -8,8 +9,8 @@ const SongList = () => {
         { title: 'Lokomotywa', id: 2},
         { title: 'Reksio', id: 3},
     ]);
-    const addSong = () => {
-         setSongs( [ ...songs, { title: 'Bonifacy i Filemon', id: uuidv4() } ] );
+    const addSong = (title) => {
+         setSongs( [ ...songs, { title: title, id: uuidv4() } ] );
     }
     return ( 
         <div className="song-list">
@@ -20,7 +21,7 @@ const SongList = () => {
                     )
                 })}
             </ul>
-            <button onClick={addSong}>Add a song</button>
+            <NewSongForm addSong={addSong}/>
         </div>
      );
 }
